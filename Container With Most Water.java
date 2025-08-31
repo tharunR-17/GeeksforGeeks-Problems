5af0354781ee4f8fcade1424cacf38b9d0edbe1c
Tharun R <123483287+tharunR-17@users.noreply.github.com>
2025-08-31 11:57:21 +0530

    Create Container With Most Water.java

---
+++ Container With Most Water.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxWater(int arr[]) {
+        int max =0;
+        int i=0, j=arr.length-1;
+        while(i<j){
+            int k = (j-i)*(Math.min(arr[i], arr[j]));
+            max=Math.max(max, k);
+            if(arr[i]>arr[j]){
+                j--;
+            }
+            else{
+                i++;
+            }
+        }
+        return max;
+    }
+}
