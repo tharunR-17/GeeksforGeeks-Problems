690204ecc9e60a69cdb23e2c5cb5253619b995c9
Tharun R <123483287+tharunR-17@users.noreply.github.com>
2025-09-05 11:55:03 +0530

    Create Sort a linked list of 0s, 1s and 2s.java

---
+++ Sort a linked list of 0s, 1s and 2s.java
@@ -0,0 +1,37 @@
+class Solution {
+    public Node segregate(Node head) {
+        // code here
+        Node zero = new Node(-1);
+        Node one = new Node(-1);
+        Node two = new Node(-1);
+        
+        Node zs = zero;
+        Node os = one;
+        Node ts = two;
+        
+        while(head!=null) {
+            Node nex = head.next;
+            head.next = null;
+            switch(head.data) {
+                case 0:
+                    zero.next = head;
+                    zero = head;
+                    break;
+                case 1:
+                    one.next = head;
+                    one = head;
+                    break;
+                case 2:
+                    two.next = head;
+                    two = head;
+                    break;
+            }
+            head = nex;
+        }
+        
+        one.next = ts.next;
+        zero.next = os.next;
+        
+        return zs.next;
+    }
+}
