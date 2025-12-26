937b579087310c178ad9894dd7923d9536c534ce
Tharun R <123483287+tharunR-17@users.noreply.github.com>
2025-12-26 11:21:55 +0530

    Create Kth Missing Positive Number in a Sorted Array.java

---
+++ Kth Missing Positive Number in a Sorted Array.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int kthMissing(int[] arr, int k) {
+        int n = arr.length;
+
+        // If kth missing is beyond the last element
+        if (arr[n - 1] - n < k) {
+            return arr[n - 1] + (k - (arr[n - 1] - n));
+        }
+
+        int low = 0, high = n - 1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            int missing = arr[mid] - (mid + 1);
+
+            if (missing < k) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+
+        // kth missing number
+        return low + k;
+    }
+}
+
