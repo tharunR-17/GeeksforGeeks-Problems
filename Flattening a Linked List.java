341fa00439733dfe8b01a6aabc9e486a5411e778
Tharun R <123483287+tharunR-17@users.noreply.github.com>
2026-01-03 15:45:27 +0530

    Create Flattening a Linked List.java

---
+++ Flattening a Linked List.java
@@ -0,0 +1,48 @@
+/*
+class Node {
+    int data;
+    Node next;
+    Node bottom;
+
+    Node(int x) {
+        data = x;
+        next = null;
+        bottom = null;
+    }
+}
+*/
+class Solution {
+    public static Node merge(Node a, Node b){
+        if(a == null){
+            return b;
+        }
+        if(b == null){
+            return a;
+        }
+        
+        Node result;
+        
+        if(a.data<b.data){
+            result = a;
+            result.bottom = merge(a.bottom, b);
+        }else{
+            result = b;
+            result.bottom = merge(a, b.bottom);
+        }
+        
+        return result;
+    }
+    
+    public Node flatten(Node root) {
+        // code here
+        if(root == null || root.next == null){
+            return root;
+        }
+        
+        root.next = flatten(root.next);
+        
+        root = merge(root, root.next);
+        return root;
+    }
+}
+
