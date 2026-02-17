8334baee40f4cb2e63a37174cac3e1bf60faa868
Tharun R <123483287+tharunR-17@users.noreply.github.com>
2026-02-17 16:30:37 +0530

    Create Maximum number of overlapping Intervals.java

---
+++ Maximum number of overlapping Intervals.java
@@ -0,0 +1,57 @@
+class Solution {
+
+    public static int overlapInt(int[][] arr) {
+
+        // code here
+
+        List<int[]> events = new ArrayList<>();
+
+ 
+
+        for (int[] log : arr) {
+
+            events.add(new int[]{log[0], +1}); // birth
+
+            events.add(new int[]{log[1], -1}); // death
+
+        }
+
+//if same number then +1 comes before -1
+
+        
+
+        Collections.sort(events, (a, b) -> {
+
+            if (a[0] == b[0])
+
+                return b[1] - a[1]; // +1 first
+
+            return a[0] - b[0];
+
+        });
+
+ 
+
+        int curr = 0;
+
+        int maxPop = 0;
+
+        
+
+ 
+
+        for (int[] e : events) {
+
+            curr += e[1];
+
+            maxPop=Math .max(curr,maxPop);
+
+        }
+
+ 
+
+        return maxPop;
+
+    }
+
+}
