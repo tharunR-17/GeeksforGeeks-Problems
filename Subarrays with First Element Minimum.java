485b64ca55e4da09ed119e531bcdf0a87e1d70c9
Tharun R <123483287+tharunR-17@users.noreply.github.com>
2026-03-10 18:34:13 +0530

    Create Subarrays with First Element Minimum.java

---
+++ Subarrays with First Element Minimum.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int countSubarrays(int[] arr) {
+        int n = arr.length;
+        Stack<Integer> st = new Stack<>();
+        int ans = 0;
+
+        for(int i = 0; i < n; i++) {
+
+            while(!st.isEmpty() && arr[st.peek()] > arr[i]) {
+                int idx = st.pop();
+                ans += i - idx;
+            }
+
+            st.push(i);
+        }
+
+        while(!st.isEmpty()) {
+            int idx = st.pop();
+            ans += n - idx;
+        }
+
+        return ans;
+    }
+}
