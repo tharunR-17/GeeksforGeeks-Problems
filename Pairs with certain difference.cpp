d36bb33ecf247a9e6709b7bd3031ee64f203d469
Tharun R <123483287+tharunR-17@users.noreply.github.com>
2026-06-02 08:59:43 +0530

    Create Pairs with certain difference.cpp

---
+++ Pairs with certain difference.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  public:
+    int sumDiffPairs(vector<int>& arr, int k) {
+        sort(arr.begin(), arr.end());
+
+        int ans = 0;
+        int n = arr.size();
+
+        int i = n - 1;
+
+        while (i > 0) {
+
+            if (arr[i] - arr[i - 1] < k) {
+
+                ans += arr[i] + arr[i - 1];
+
+                i -= 2;
+            }
+            else {
+
+                i--;
+            }
+        }
+
+        return ans;
+    }
+};
